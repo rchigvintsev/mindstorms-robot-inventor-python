b07fda9ea19657644e83448a13c917cacf8ad9c4
Роман Чигвинцев <rchigvintsev@gmail.com>
2023-02-23 23:03:58 +0300

    Remove conflicting Timer import

--- charlie/crazy-shopper.py
+++ charlie/crazy-shopper.py
@@ -1,5 +1,5 @@
 from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
-from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.control import wait_for_seconds, wait_until
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 
 from utime import ticks_diff, ticks_ms
